c1a4e7691238ab496dba23d3803942fd1327ef3c
SaBowers2002 <108242682+SaBowers2002@users.noreply.github.com>
2023-07-13 13:26:37 -0400

    Add files via upload

---
+++ Final Project/script.js
@@ -0,0 +1,58 @@
+function displayMealPlan() {
+    // MONDAY
+    var mondayBreakfast = document.getElementById('monday-breakfast').value;
+    var mondaySnack = document.getElementById('monday-snack').value;
+    var mondayLunch = document.getElementById('monday-lunch').value;
+    var mondaySnack2 = document.getElementById('monday-snack2').value;
+    var mondayDinner = document.getElementById('monday-dinner').value;
+
+    // TUESDAY
+    var tuesdayBreakfast = document.getElementById('tuesday-breakfast').value;
+    var tuesdaySnack = document.getElementById('tuesday-snack').value;
+    var tuesdayLunch = document.getElementById('tuesday-lunch').value;
+    var tuesdaySnack2 = document.getElementById('tuesday-snack2').value;
+    var tuesdayDinner = document.getElementById('tuesday-dinner').value;
+
+    // WEDNESDAY
+    var wednesdayBreakfast = document.getElementById('wednesday-breakfast').value;
+    var wednesdaySnack = document.getElementById('wednesday-snack').value;
+    var wednesdayLunch = document.getElementById('wednesday-lunch').value;
+    var wednesdaySnack2 = document.getElementById('wednesday-snack2').value;
+    var wednesdayDinner = document.getElementById('wednesday-dinner').value;
+
+    // THURSDAY
+    var thursdayBreakfast = document.getElementById('thursday-breakfast').value;
+    var thursdaySnack = document.getElementById('thursday-snack').value;
+    var thursdayLunch = document.getElementById('thursday-lunch').value;
+    var thursdaySnack2 = document.getElementById('thursday-snack2').value;
+    var thursdayDinner = document.getElementById('thursday-dinner').value;
+
+    // FRIDAY
+    var fridayBreakfast = document.getElementById('friday-breakfast').value;
+    var fridaySnack = document.getElementById('friday-snack').value;
+    var fridayLunch = document.getElementById('friday-lunch').value;
+    var fridaySnack2 = document.getElementById('friday-snack2').value;
+    var fridaytDinner = document.getElementById('friday-dinner').value;
+
+    // SATURDAY 
+    var saturdayBreakfast = document.getElementById('saturday-breakfast').value;
+    var saturdaySnack = document.getElementById('saturday-snack').value;
+    var saturdayLunch = document.getElementById('saturday-lunch').value;
+    var saturdaySnack2 = document.getElementById('saturday-snack2').value;
+    var saturdayDinner = document.getElementById('saturday-dinner').value;
+
+    // SUNDAY
+    var sundayBreakfast = document.getElementById('sunday-breakfast').value;
+    var sundaySnack = document.getElementById('sunday-snack').value;
+    var sundayLunch = document.getElementById('sunday-lunch').value;
+    var sundaySnack2 = document.getElementById('sunday-snack2').value;
+    var sundayDinner = document.getElementById('sunday-dinner').value;
+
+    // -------------------------------
+    //TABLE
+
+
+    // Go to the new page :)
+    window.location.href = 'submissionForm.html';
+
+}
